f4b2726369413db5911b396bb20fbcf9e3077448
greg <gregsohl@gmail.com>
2017-06-10 14:04:21 -0500

    Added status bar and disabling during drawing.

--- Turtle-Graphics-Example/DemoForm.Designer.cs
+++ Turtle-Graphics-Example/DemoForm.Designer.cs
@@ -33,8 +33,12 @@
 			this.buttonReset = new System.Windows.Forms.Button();
 			this.buttonShowHideTurtle = new System.Windows.Forms.Button();
 			this.panel1 = new System.Windows.Forms.Panel();
+			this.statusBar = new System.Windows.Forms.StatusStrip();
 			this.panelSurface = new System.Windows.Forms.Panel();
+			this.Coordinates = new System.Windows.Forms.ToolStripStatusLabel();
+			this.Angle = new System.Windows.Forms.ToolStripStatusLabel();
 			this.panel1.SuspendLayout();
+			this.statusBar.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// buttonDraw
@@ -89,14 +93,37 @@
 			this.panel1.Size = new System.Drawing.Size(101, 720);
 			this.panel1.TabIndex = 4;
 			// 
+			// statusBar
+			// 
+			this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Coordinates,
+            this.Angle});
+			this.statusBar.Location = new System.Drawing.Point(101, 698);
+			this.statusBar.Name = "statusBar";
+			this.statusBar.Size = new System.Drawing.Size(912, 22);
+			this.statusBar.TabIndex = 6;
+			this.statusBar.Text = "statusStrip1";
+			// 
 			// panelSurface
 			// 
 			this.panelSurface.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.panelSurface.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.panelSurface.Location = new System.Drawing.Point(101, 0);
 			this.panelSurface.Name = "panelSurface";
-			this.panelSurface.Size = new System.Drawing.Size(912, 720);
-			this.panelSurface.TabIndex = 5;
+			this.panelSurface.Size = new System.Drawing.Size(912, 698);
+			this.panelSurface.TabIndex = 7;
+			// 
+			// Coordinates
+			// 
+			this.Coordinates.Name = "Coordinates";
+			this.Coordinates.Size = new System.Drawing.Size(109, 17);
+			this.Coordinates.Text = "toolStripStatusLabel1";
+			// 
+			// Angle
+			// 
+			this.Angle.Name = "Angle";
+			this.Angle.Size = new System.Drawing.Size(109, 17);
+			this.Angle.Text = "toolStripStatusLabel2";
 			// 
 			// DemoForm
 			// 
@@ -104,12 +131,16 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1013, 720);
 			this.Controls.Add(this.panelSurface);
+			this.Controls.Add(this.statusBar);
 			this.Controls.Add(this.panel1);
 			this.Name = "DemoForm";
 			this.Text = "Nakov.TurtleGraphics - Demo";
 			this.Load += new System.EventHandler(this.DemoForm_Load);
 			this.panel1.ResumeLayout(false);
+			this.statusBar.ResumeLayout(false);
+			this.statusBar.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -120,6 +151,9 @@
 		private System.Windows.Forms.Button buttonReset;
 		private System.Windows.Forms.Button buttonShowHideTurtle;
 		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.StatusStrip statusBar;
+		private System.Windows.Forms.ToolStripStatusLabel Coordinates;
+		private System.Windows.Forms.ToolStripStatusLabel Angle;
 		private System.Windows.Forms.Panel panelSurface;
 	}
 }
--- Turtle-Graphics-Example/DemoForm.cs
+++ Turtle-Graphics-Example/DemoForm.cs
@@ -19,48 +19,73 @@ namespace Turtle_Graphics_Example
 
 		private void buttonDraw_Click(object sender, EventArgs e)
 		{
-			// Assign a delay to visualize the drawing process
-			Turtle.Delay = 150;
+			Enabled = false;
+			try
+			{
 
-			// Draw a equilateral triangle
-			Turtle.PenColor = Color.Green;
-			Turtle.Rotate(30);
-			Turtle.Forward(200);
-			Turtle.Rotate(120);
-			Turtle.Forward(200);
-			Turtle.Rotate(120);
-			Turtle.Forward(200);
+				// Assign a delay to visualize the drawing process
+				Turtle.Delay = 150;
 
-			// Draw a line in the triangle
-			Turtle.Rotate(-30);
-			Turtle.PenUp();
-			Turtle.Backward(50);
-			Turtle.PenDown();
-			Turtle.PenColor = Color.Red;
-			Turtle.PenSize = 5;
-			Turtle.Backward(100);
-			Turtle.PenColor = Turtle.DEFAULT_COLOR;
-			Turtle.PenSize = Turtle.DEFAULT_PEN_SIZE;
-			Turtle.PenUp();
-			Turtle.Forward(150);
-			Turtle.PenDown();
-			Turtle.Rotate(30);
+				// Draw a equilateral triangle
+				Turtle.PenColor = Color.Green;
+				Turtle.Rotate(30);
+				Turtle.Forward(200);
+				Turtle.Rotate(120);
+				Turtle.Forward(200);
+				Turtle.Rotate(120);
+				Turtle.Forward(200);
+
+				// Draw a line in the triangle
+				Turtle.Rotate(-30);
+				Turtle.PenUp();
+				Turtle.Backward(50);
+				Turtle.PenDown();
+				Turtle.PenColor = Color.Red;
+				Turtle.PenSize = 5;
+				Turtle.Backward(100);
+				Turtle.PenColor = Turtle.DEFAULT_COLOR;
+				Turtle.PenSize = Turtle.DEFAULT_PEN_SIZE;
+				Turtle.PenUp();
+				Turtle.Forward(150);
+				Turtle.PenDown();
+				Turtle.Rotate(30);
+			}
+			finally
+			{
+				Enabled = true;
+			}
+
+			UpdateStatusBar();
 		}
 
 		private void buttonDrawSpiral_Click(object sender, EventArgs e)
 		{
-			Turtle.PenColor = Color.Red;
-			Turtle.Delay = 50;
-			for (int i = 0; i < 25; i++)
+			Enabled = false;
+
+			try
 			{
-				Turtle.Forward(i * 5);
-				Turtle.Rotate(30 + i);
+				Turtle.PenColor = Color.Red;
+				Turtle.Delay = 50;
+			
+				for (int i = 0; i < 25; i++)
+				{
+					Turtle.Forward(i * 5);
+					Turtle.Rotate(30 + i);
+				}
+
 			}
+			finally
+			{
+				Enabled = true;
+			}
+
+			UpdateStatusBar();
 		}
 
 		private void buttonReset_Click(object sender, EventArgs e)
 		{
 			Turtle.Reset();
+			UpdateStatusBar();
 		}
 
 		private void buttonShowHideTurtle_Click(object sender, EventArgs e)
@@ -80,6 +105,13 @@ namespace Turtle_Graphics_Example
 		private void DemoForm_Load(object sender, EventArgs e)
 		{
 			Turtle.Init(panelSurface);
+			UpdateStatusBar();
+		}
+
+		private void UpdateStatusBar()
+		{
+			statusBar.Items["Coordinates"].Text = string.Format("X:{0} Y:{1}", Turtle.X, Turtle.Y);
+			statusBar.Items["Angle"].Text = string.Format("Angle:{0}", Turtle.Angle);
 		}
 	}
 }
